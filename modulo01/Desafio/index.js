aedfd0a655e853c12b3439c3860a229b02f1e243
murilozelic <murilozelic@gmail.com>
2019-10-15 16:32:01 -0300

    Desafio finalizado

--- modulo01/Desafio/index.js
+++ modulo01/Desafio/index.js
@@ -2,8 +2,87 @@ const express = require('express');
 
 const server = express();
 
-server.get('', (req, res) => {
-  return res.send('Aasdasdaso');
+server.use(express.json());
+
+const projects = [];
+let count = 0;
+
+// Crie um middleware global chamado em todas requisições que imprime (console.log) 
+// uma contagem de quantas requisições foram feitas na aplicação até então;
+server.use((req, res, next) => {
+
+  console.log(`Método ${req.method}; Contagem: ${count += 1}`);
+
+  next();
+})
+
+// Crie um middleware que será utilizado em todas rotas que recebem o ID do projeto
+// nos parâmetros da URL que verifica se o projeto com aquele ID existe. Se não 
+// existir retorne um erro, caso contrário permita a requisição continuar 
+// normalmente;
+function checkProjectId(req, res, next) {
+  const { id } = req.params;
+
+  if (!projects[id]) {
+    return res.status(400).json({ error: 'Project does not exists!' })
+  }
+
+  return next();
+}
+
+// POST /projects
+// A rota deve receber id e title dentro corpo de cadastrar um novo projeto dentro 
+// de um array no seguinte formato: { id: "1", title: 'Novo projeto', tasks: [] }; 
+// Certifique-se de enviar tanto o ID quanto o título do projeto no formato string 
+// com àspas duplas.
+server.post('/projects', (req, res) => {
+  const { id } = req.body;
+  const { title } = req.body;
+  const tasks = [];
+
+  projects[id] = { id, title, tasks };
+  return res.json(projects);
 });
+
+// GET /projects
+// Rota que lista todos projetos e suas tarefas;
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+})
+
+// PUT /projects/:id
+// A rota deve alterar apenas o título do 
+// projeto com o id presente nos parâmetros da rota;
+server.put('/projects/:id', checkProjectId, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  projects[id].title = title;
+  res.json(projects);
+});
+
+// DELETE /projects/:id
+// A rota deve deletar o projeto com o id presente nos 
+// parâmetros da rota;
+server.delete('/projects/:id', checkProjectId, (req, res) => {
+  const { id } = req.params;
+
+  projects.splice(id, 1);
+
+  res.json(projects);
+});
+
+// POST /projects/:id/tasks
+// A rota deve receber um campo title e armazenar uma 
+// nova tarefa no array de tarefas de um projeto específico escolhido através do 
+// id presente nos parâmetros da rota;
+server.post('/projects/:id/tasks', checkProjectId, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  projects[id].tasks.push(title);
+
+  res.json(projects);
+})
 
 server.listen(3000);
